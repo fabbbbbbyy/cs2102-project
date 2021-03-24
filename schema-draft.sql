6f1784113cbe547916f034eb181c9e9c07dcc379
Kevin Lim <lyskevin@gmail.com>
2021-03-24 15:12:53 +0800

    update schema

--- schema-draft.sql
+++ schema-draft.sql
@@ -1,14 +1,17 @@
-/* This schema follows the template ER diagram from left to right as much as possible. 
+/* This schema follows the template ER diagram from left to right as much as possible.
 
    The ordering for attributes are in lexographical order and primary and foreign keys are 
-   denoted at the bottom for consistent styling across tables. 
+   denoted at the bottom for consistent styling across tables.
 
    If a table is a combined table between entities, we will have the attributes of the overarching
    entity in lexographical order, followed by the attributes of the 2nd/3rd/etc.. entity similarly
-   in lexographical order below it. 
+   in lexographical order below it.
 
    Attribute types are subject to change. */
 
+/* Decided to use int data type with auto increment for primary keys */
+
+/* (CORRECT) */
 create table Course_Packages (
     package_id integer primary key,
     name text,
@@ -18,6 +21,7 @@ create table Course_Packages (
     sale_end_date date
 );
 
+/* (CORRECT) */
 create table Credit_Cards (
   number integer primary key,
   expiry_date date,
@@ -25,25 +29,29 @@ create table Credit_Cards (
   cvv integer
 );
 
+/* (CORRECT) */
+/* Application specs and functions 3 and 4 seem to imply a one-to-one relationship */
 create table Customers (
   cust_id integer primary key,
   address text,
   name text,
   email text,
   phone text,
-  credit_card_number integer unique not null references Credit_Cards
+  number integer unique not null references Credit_Cards
 );
 
+/* (CORRECT) */
 create table Employees (
+  	eid integer primary key,
     address text,
     depart_date date,
-    eid integer primary key,
     email text,
     join_date date,
     name text,
-    phone text,
+    phone text
 );
 
+/* (CORRECT) */
 create table Employee_Pay_Slips (
     amount numeric, 
     eid integer,
@@ -51,10 +59,11 @@ create table Employee_Pay_Slips (
     num_work_hours integer,
     payment_date date,
     primary key(payment_date, eid),
-    foreign key(eid) references Employees  
+    foreign key(eid) references Employees
         on delete cascade
 );
 
+/* (CORRECT) */
 /* hourly_rate not null because any row in Part_Time_Employees must exist */
 create table Part_Time_Employees (
     eid integer primary key references Employees
@@ -62,6 +71,7 @@ create table Part_Time_Employees (
     hourly_rate numeric not null
 );
 
+/* (CORRECT) */
 /* monthly_salary not null because any row in Full_Time_Employees must exist */
 create table Full_Time_Employees (
     eid integer primary key references Employees
@@ -69,40 +79,56 @@ create table Full_Time_Employees (
     monthly_salary numeric not null
 );
 
+/* (CORRECT) */
+/* Administrator is a full time emp*/
 create table Administrators (
   eid integer primary key references Full_Time_Employees
       on delete cascade
 );
 
+/* (CORRECT) */
+/* Manager is a full time emp*/
 create table Managers (
   eid integer primary key references Full_Time_Employees
       on delete cascade
 );
 
+/* (CORRECT) */
 create table Course_Areas (
     eid integer not null references Managers,
     name text primary key
 );
 
+/* (CORRECT) */
+/*Instructor is a employee*/
 create table Instructors (
     eid integer references Employees
   			on delete cascade,
-    name text references Course_Areas,
+    name text references Course_Areas, /*specialize relationship*/
     primary key(eid, name)
 );
 
+/* (CORRECT) */
 create table Part_Time_Instructors (
-  eid integer primary key references Instructors
-      references Part_Time_Employees
-      on delete cascade
+  eid integer references Part_Time_Employees
+      on delete cascade,
+  name text,
+  foreign key(eid, name) references Instructors
+      on delete cascade,
+  primary key(eid, name)
 );
 
+/* (CORRECT) */
 create table Full_Time_Instructors (
-  eid integer primary key references Instructors
-      references Full_Time_Employees
-      on delete cascade
+  eid integer references Full_Time_Employees
+      on delete cascade,
+  name text,
+  foreign key(eid, name) references Instructors
+      on delete cascade,
+  primary key(eid, name)
 );
 
+/* (CORRECT) */
 create table Courses (
     course_id integer primary key,
     description text,
@@ -111,14 +137,16 @@ create table Courses (
     title text
 );
 
+/* (CORRECT) */
 /*encapsulates Handles weak entity and Has relationship*/
 create table Course_Offerings (
     course_id integer references Courses
   		on delete cascade,
-    aeid integer not null references Administrators(eid),
+    launch_date date,
+    admin_eid integer not null references Administrators
+        on delete cascade,
     end_date date,
     fees numeric,
-    launch_date date, 
     registration_deadline date,
     seating_capacity integer, 
     start_date date,
@@ -126,60 +154,78 @@ create table Course_Offerings (
     primary key(course_id, launch_date)
 );
 
+/* (CORRECT) */
 create table Rooms (
     rid integer primary key,
     seating_capacity integer,
     location text
 );
 
-/*Encapuslates Consists weak entity and Conducts(ternary relationship with Rooms and Instructors) relationship*/
+/* (CORRECT) */
 create table Course_Offering_Sessions (
     sid integer,
     session_date date,
-  	instructor_id integer,
-  	specialization text,
     start_time timestamp,
     end_time timestamp,
-    rid integer not null references Room,
     launch_date date not null,
     course_id integer not null,
-    primary key (sid, launch_date, course_id, instructor_id, specialization),
+    primary key (sid, launch_date, course_id),
     foreign key(course_id, launch_date) references Course_Offerings
-          on delete cascade,
-  	foreign key(instructor_id, specialization) references Instructors(eid, name)
+          on delete cascade
 );
 
+/* (CORRECT) */
+create table Conducts (
+    rid integer references Rooms
+        on delete cascade,
+  	eid integer,
+    name text,
+  	sid integer not null,
+  	launch_date date not null,
+  	course_id integer not null,
+    foreign key(eid, name) references Instructors
+        on delete cascade,
+  	foreign key(sid, launch_date, course_id) references Course_Offering_Sessions
+        on delete cascade,
+    primary key(rid, eid, sid)
+);
+
+/* (CORRECT) */
 create table Cancels (
     cancel_date date,
-    cust_id integer references Customers,
+    cust_id integer references Customers
+  			on delete cascade,
     launch_date date,
     package_credit integer,
     refund_amt integer,
     sid integer,
-  	specialization text,
-  	instructor_id integer,
   	course_id integer,
-    primary key(cust_id, cancel_date, launch_date, sid, course_id, instructor_id, specialization),
-    foreign key(sid, launch_date, course_id, instructor_id, specialization) references Course_Offering_Sessions 
+    primary key(cust_id, cancel_date, launch_date, sid, course_id),
+    foreign key(sid, launch_date, course_id) references Course_Offering_Sessions
+  			on delete cascade
 );
 
+/* (CORRECT) */
 create table Registers (
-    cust_id integer references Customers,
+    cust_id integer references Customers
+    		on delete cascade,
     launch_date date,
     register_date date,
     sid integer,
-  	instructor_id integer,
-  	specialization text,
   	course_id integer,
-    primary key(cust_id, register_date,  launch_date, sid, course_id, instructor_id, specialization),
-    foreign key(sid, launch_date, course_id, instructor_id, specialization) references Course_Offering_Sessions
+    primary key(cust_id, register_date,  launch_date, sid, course_id),
+    foreign key(sid, launch_date, course_id) references Course_Offering_Sessions
+  	  	on delete cascade
 );
 
+/* (CORRECT) */
 /* num_remaining_redemptions not null because any transaction in Buys has already occurred */
 create table Buys (
-    cust_id integer references Customers,
-    package_id integer references Course_Packages,
-    purchase_date date,
+    cust_id integer references Customers
+      on delete cascade,
+    package_id integer references Course_Packages
+      on delete cascade,
+    purchase_date date not null,
     num_remaining_redemptions integer not null,
     primary key(cust_id, purchase_date, package_id)
 );
@@ -189,12 +235,13 @@ create table Redeems (
     redemption_date date,
     launch_date date,
   	course_id integer,
-  	instructor_id integer,
-  	specialization text,
     purchase_date date,
     package_id integer,
     sid integer,
-    primary key(cust_id, sid, launch_date, package_id, redemption_date),
-    foreign key(cust_id, purchase_date, package_id) references Buys,
-    foreign key(sid, launch_date, course_id, instructor_id, specialization) references Course_Offering_Sessions 
+    primary key(cust_id, redemption_date, launch_date, course_id, purchase_date, package_id, sid),
+    foreign key(cust_id, purchase_date, package_id) references Buys
+        on delete cascade,
+    foreign key(sid, launch_date, course_id) references Course_Offering_Sessions
+        on delete cascade
 );
+
