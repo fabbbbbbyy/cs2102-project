6b72d4207f066601d28c4d7e795aa98ef879c8d7
siddarth2824 <siddarth2824@gmail.com>
2021-04-01 12:56:32 +0800

    Add triggers and functions

--- server/init-scripts/1-schema.sql
+++ server/init-scripts/1-schema.sql
@@ -52,7 +52,7 @@ create table Employees (
     depart_date date,
     email text,
     join_date date,
-    name text,
+    employee_name text,
     phone text
 );
 
@@ -107,10 +107,10 @@ create table Course_Areas (
 /* (CORRECT) */
 /*Instructor is a employee*/
 create table Instructors (
-    eid integer references Employees
+    instructor_id integer references Employees
   			on delete cascade,
-    name text references Course_Areas, /*specialize relationship*/
-    primary key(eid, name)
+    course_area_name text references Course_Areas, /*specialize relationship*/
+    primary key(instructor_id, course_area_name)
 );
 
 /* (CORRECT) */
@@ -138,7 +138,7 @@ create table Courses (
     course_id serial primary key,
     description text,
     duration integer,
-    name text not null references Course_Areas,
+    course_area_name text not null references Course_Areas,
     title text
 );
 
@@ -156,7 +156,8 @@ create table Course_Offerings (
     seating_capacity integer, 
     start_date date,
     target_number_registrations integer,
-    primary key(course_id, launch_date)
+    primary key(course_id, launch_date),
+    check(end_date >= start_date)
 );
 
 /* (CORRECT) */
@@ -170,29 +171,32 @@ create table Rooms (
 create table Course_Offering_Sessions (
     sid serial,
     session_date date,
-    start_time timestamp,
-    end_time timestamp,
+    start_time_hour integer,
+    end_time_hour integer,
     launch_date date not null,
     course_id integer not null,
     primary key (sid, launch_date, course_id),
     foreign key(course_id, launch_date) references Course_Offerings
-          on delete cascade
+          on delete cascade,
+
+    check (extract(dow from session_date) not in (0, 6)),
+  	check (end_time_hour > start_time_hour)
 );
 
 /* (CORRECT) */
 create table Conducts (
     rid integer references Rooms
         on delete cascade,
-  	eid integer,
-    name text,
+  	instructor_id integer,
+    course_area_name text,
   	sid integer,
   	launch_date date not null,
   	course_id integer not null,
-    foreign key(eid, name) references Instructors
+    foreign key(instructor_id, course_area_name) references Instructors
         on delete cascade,
   	foreign key(sid, launch_date, course_id) references Course_Offering_Sessions
         on delete cascade,
-    primary key(rid, eid, sid)
+    primary key(rid, instructor_id, sid)
 );
 
 /* (CORRECT) */
@@ -251,3 +255,180 @@ create table Redeems (
 );
 
 /* SQL or PL/pgSQL routines for trigger implementations */
+/* A full time employee MUST either be a manager or an admin but not both */
+CREATE OR REPLACE FUNCTION manager_employee_verification_func() 
+RETURNS TRIGGER AS $$
+DECLARE
+	num_same_eid_records integer;
+    num_instructor_same_eid_records integer;
+BEGIN
+	SELECT COUNT(*) INTO num_same_eid_records
+    FROM Administrators A
+    WHERE A.eid = NEW.eid;
+
+    SELECT COUNT(*) INTO num_instructor_same_eid_records
+    FROM Instructors
+    WHERE instructor_id = NEW.eid;
+  
+	IF num_same_eid_records > 0 THEN
+  	    RAISE EXCEPTION 'This employee is already an administrator, and therefore cannot be a manager.';
+    END IF;
+    
+    IF num_instructor_same_eid_records > 0 THEN
+        RAISE EXCEPTION 'This employee is already an instructor, and therefore cannot be a manager.';
+    END IF; 
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER manager_employee_verification
+BEFORE INSERT OR UPDATE ON Managers
+FOR EACH ROW EXECUTE FUNCTION manager_employee_verification_func();
+
+CREATE OR REPLACE FUNCTION administrator_employee_verification_func() 
+RETURNS TRIGGER AS $$
+DECLARE
+	num_same_eid_records integer;
+    num_instructor_same_eid_records integer;
+BEGIN
+	SELECT COUNT(*) INTO num_same_eid_records
+    FROM Managers M
+    WHERE M.eid = NEW.eid;
+
+    SELECT COUNT(*) INTO num_instructor_same_eid_records
+    FROM Instructors
+    WHERE instructor_id = NEW.eid;
+  
+	IF num_same_eid_records > 0 THEN
+  	    RAISE EXCEPTION 'This employee is already a manager, and therefore cannot be an administrator.';
+    END IF;
+
+    IF num_instructor_same_eid_records > 0 THEN
+        RAISE EXCEPTION 'This employee is already an instructor, and therefore cannot be a administrator.';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER administrator_employee_verification
+BEFORE INSERT OR UPDATE ON Administrators
+FOR EACH ROW EXECUTE FUNCTION administrator_employee_verification_func();
+
+CREATE OR REPLACE FUNCTION instructor_employee_verification_func() 
+RETURNS TRIGGER AS $$
+DECLARE
+	num_manager_same_eid_records integer;
+    num_administrator_same_eid_records integer;
+BEGIN
+	SELECT COUNT(*) INTO num_manager_same_eid_records
+    FROM Managers
+    WHERE eid = NEW.instructor_id;
+
+    SELECT COUNT(*) INTO num_administrator_same_eid_records
+    FROM Administrators
+    WHERE eid = NEW.instructor_id;
+  
+	IF num_manager_same_eid_records > 0 THEN
+  	    RAISE EXCEPTION 'This employee is already a manager, and therefore cannot be an instructor.';
+    END IF;
+
+    IF num_administrator_same_eid_records > 0 THEN
+        RAISE EXCEPTION 'This employee is already an administrator, and therefore cannot be an instructor.';
+    END IF;
+  	
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER instructor_employee_verification
+BEFORE INSERT OR UPDATE ON Instructors
+FOR EACH ROW EXECUTE FUNCTION instructor_employee_verification_func();
+
+/* Instructor MUST be either a full time or part time instructor but not both (Siddarth)*/
+CREATE OR REPLACE FUNCTION check_instructor_is_not_full_time_instructor()
+RETURNS TRIGGER AS $$
+DECLARE
+	count_of_full_time_instr integer;
+BEGIN
+    SELECT count(*) INTO count_of_full_time_instr
+    FROM Full_Time_Instructors
+    WHERE NEW.eid = eid;
+  
+    IF count_of_full_time_instr > 0 THEN
+        RAISE EXCEPTION 'This instructor is already a full time instructor, and therefore cannot be a part time instructor.';
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_instructor_is_not_full_time_instructor_trigger
+BEFORE INSERT OR UPDATE ON Part_Time_Instructors
+FOR EACH ROW EXECUTE FUNCTION check_instructor_is_not_full_time_instructor();
+
+
+CREATE OR REPLACE FUNCTION check_instructor_is_not_part_time_instructor()
+RETURNS TRIGGER AS $$
+DECLARE
+	count_of_full_time_instr integer;
+BEGIN
+	SELECT count(*) INTO count_of_full_time_instr
+    FROM Part_Time_Instructors
+    WHERE NEW.eid = eid;
+  
+    IF count_of_full_time_instr > 0 THEN
+        RAISE EXCEPTION 'This instructor is already a part time instructor, and therefore cannot be a full time instructor.';
+    ELSE
+        RETURN NEW;
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_instructor_is_not_part_time_instructor_trigger
+BEFORE INSERT OR UPDATE ON Full_Time_Instructors
+FOR EACH ROW EXECUTE FUNCTION check_instructor_is_not_part_time_instructor();
+
+/* Employee can be either a full_time or part_time employee but not both */ 
+CREATE OR REPLACE FUNCTION part_time_employee_verification_func() 
+RETURNS TRIGGER AS $$
+DECLARE
+	num_same_eid_records integer;
+BEGIN
+	SELECT COUNT(*) INTO num_same_eid_records
+  FROM Full_Time_Employees FTE
+  WHERE FTE.eid = NEW.eid;
+  
+	IF num_same_eid_records > 0 THEN
+  	RAISE EXCEPTION 'This employee is already a full time employee, and therefore cannot be a part time employee.';
+  ELSE 
+  	RETURN NEW;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER part_time_employee_verification
+BEFORE INSERT OR UPDATE ON Part_Time_Employees
+FOR EACH ROW EXECUTE FUNCTION part_time_employee_verification_func();
+
+CREATE OR REPLACE FUNCTION full_time_employee_verification_func() 
+RETURNS TRIGGER AS $$
+DECLARE
+	num_same_eid_records integer;
+BEGIN
+	SELECT COUNT(*) INTO num_same_eid_records
+  FROM Part_Time_Employees PTE
+  WHERE PTE.eid = NEW.eid;
+  
+	IF num_same_eid_records > 0 THEN
+  	RAISE EXCEPTION 'This employee is already a part time employee, and therefore cannot be a full time employee.';
+  ELSE 
+  	RETURN NEW;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER full_time_employee_verification
+BEFORE INSERT OR UPDATE ON Full_Time_Employees
+FOR EACH ROW EXECUTE FUNCTION full_time_employee_verification_func();
